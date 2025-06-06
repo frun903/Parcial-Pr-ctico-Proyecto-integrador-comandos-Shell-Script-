aa640bd633a5de9b7e34646f6548e4bfbef73763
frun903 <frun903@gmail.com>
2025-06-05 21:11:55 -0300

    Agregue el punto de listado x direccion

--- integrador.sh
+++ integrador.sh
@@ -1,53 +1,117 @@
 #!/bin/bash
 
+#Declaro los Colores
 COLOR_ROJO="\e[1;31m"
+COLOR_VERDE="\e[1;32m"
+
+#Declaro la negrita
 COLOR_RESET="\e[0m"
 
+
+# --------------------------------------------------------------------------------------------------------------------------------------
+# Función: listar_directorio_actual
+# ---------------------------------------------------------------------------------------------------------------------------------------
+# Esta funcion tiene 4 opciones 
 # Listar directorio actual en linea
 # Listar directorio actual y ocultos
 # Listar directorio actual en arbol
 # Listar directorio actual en lista 
 listar_directorio_actual(){
+    clear
     while true; do
+        
         echo -e "${COLOR_ROJO}Ingrese Opciones Para listar el directorio Actual!${COLOR_RESET}"
-        echo "1) Listar directorio actual"
-        echo "2) Listar directorio actual y archivos ocultos"
-        echo "3) Listar directorios y subdirectorios en formato árbol"
-        echo "4) Listar directorios y permisos en lista "
-        echo "5) Volver al menu principal"
+        echo "1) Listar contenido (sin ocultos)"
+        echo "2) Listar con archivos ocultos"
+        echo "3) Listar en formato árbol"
+        echo "4) Listar en formato largo (y con permisos)"
+        echo "5) Volver"
+        echo "Selecione una opcion 1 a 5: "
         read opciones
 
         case $opciones in 
-            1) ls ;;
+            1) ls;;
             2) ls -a ;;
             3) tree ;;
             4) ls -l;;
-            5) echo "Saliendo..."; break ;;
+            5) echo  echo -e "${COLOR_VERDE}Regresando...${COLOR_RESET}"; sleep 1 ;break ;;
             *) echo "Opción no válida, Reintente" ;break;
         esac
+
+       echo "ENTER para continuar..." 
+       read dummy
+       clear
     done
 }
 
 
-listar_directorioXdireccion(){
-    echo -e "Ingrese direccion"
 
+
+
+# ------------------------------------------------------------------------------------------------------------------------
+# Función: listar_directorio_por_ruta
+# ------------------------------------------------------------------------------------------------------------------------
+# Esta funcion tiene las mismas 4 opciones que el anterior pero en este caso el usuario debe poner la direcccion
+# Listar directorio actual en linea
+# Listar directorio actual y ocultos
+# Listar directorio actual en arbol
+# Listar directorio actual en lista 
+
+listar_directorioXdireccion(){
+     clear
+      echo "Ingrese la ruta del directorio (ENTER = actual): ${COLOR_RESET}" 
+      read ruta
+     ruta=${ruta:-.}
+      #ruta de ejemplo /home/frang/Escritorio/D&D
+
+      #Manejjo de errores preliminae quiero hacer un ounto log
+      if [[ ! -d "$ruta" ]]; then
+          echo -e "${COLOR_ROJO}Error: '$ruta' no es un directorio valido.${COLOR_RESET}"
+          echo "ENTER para volver..." 
+          read dummy
+          return
+      fi
+
+      while true; do
+            echo -e "${COLOR_ROJO}-- Listar en '$ruta' --${COLOR_RESET}"
+            echo "1) Listar contenido (sin ocultos)"
+            echo "2) Listar con archivos ocultos"
+            echo "3) Listar en formato árbol"
+            echo "4) Listar en formato largo (permisos)"
+            echo "5) Volver"
+            read -p "Seleccione una opción [1-5]: " opcion2
+
+            case $opcion2 in 
+                1) ls "$ruta" ;;
+                2) ls -a "$ruta" ;;
+                3) tree "$ruta" ;;
+                4) ls -l "$ruta" ;;
+                5) 
+                    echo -e "${COLOR_VERDE}Regresando...${COLOR_RESET}"
+                    sleep 0.5
+                    break
+                    ;;
+                *) echo -e "${COLOR_ROJO}Opción no válida.${COLOR_RESET}" ;;
+            esac
+
+            echo
+            read -p "ENTER para continuar..." dummy
+            clear
+      done
 }
 
 
 
-# Listar Directorio
 
-# Listar directorio actual ()
-# Listar directorio actual y ocultos
-# Listar directorio actual en arbol
+# Menu principal 
 while true; do
+  clear
   echo -e "${COLOR_ROJO}Ingrese Opciones!${COLOR_RESET}"
   read opciones
 
   case $opciones in 
     1) listar_directorio_actual ;;
-    2) echo "Listado por direccion";;
+    2) listar_directorioXdireccion ;;
     3) echo "Punto 2" ;;
     4) echo "Punto 3";;
     5) echo -e "${COLOR_ROJO}Terminando Ejecucion"; break ;;
